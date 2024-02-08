bfb26e7cb1e62494604b5bdd9c1da1840b5a348e
Aleksandra Sikora <1139947+alsikora@users.noreply.github.com>
2024-02-08 18:31:42 +0100

    Input form

---
+++ src/components/tubeInputForm/TubeInputForm.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import styles from './TubeInputForm.module.css'
+
+const TubeInputForm = ({ onAddTube }) => {
+  const [id, setId] = useState('');
+  const [age, setAge] = useState('');
+  const [company, setCompany] = useState('');
+  const [cityDistrict, setCityDistrict] = useState('');
+  const [visionDefect, setVisionDefect] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddTube({ id, patient: { age, company, cityDistrict, visionDefect } });
+    setId('');
+    setAge('');
+    setCompany('');
+    setCityDistrict('');
+    setVisionDefect('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <input
+        type="number"
+        placeholder="Id"
+        value={id}
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Age"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Company"
+        value={company}
+        onChange={(e) => setCompany(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="City District"
+        value={cityDistrict}
+        onChange={(e) => setCityDistrict(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Vision Defect"
+        value={visionDefect}
+        onChange={(e) => setVisionDefect(e.target.value)}
+      />
+      <button type="submit">Add Tube</button>
+    </form>
+  );
+};
+
+export default TubeInputForm;
